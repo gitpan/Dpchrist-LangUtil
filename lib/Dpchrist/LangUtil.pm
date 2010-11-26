54f7270739372f913dc93b3de39412ccb7494914
David Paul Christensen <dpchrist@holgerdanske.com>
2010-11-26 21:29:25 +0000

    Import of DPCHRIST/Dpchrist-LangUtil-1.011 from CPAN.
    gitpan-cpan-distribution: Dpchrist-LangUtil
    gitpan-cpan-version:      1.011
    gitpan-cpan-path:         DPCHRIST/Dpchrist-LangUtil-1.011.tar.gz
    gitpan-cpan-author:       DPCHRIST
    gitpan-cpan-maturity:     released

--- lib/Dpchrist/LangUtil.pm
+++ lib/Dpchrist/LangUtil.pm
@@ -1,5 +1,5 @@
 #######################################################################
-# $Id: LangUtil.pm,v 1.10 2010-11-25 19:33:31 dpchrist Exp $
+# $Id: LangUtil.pm,v 1.11 2010-11-26 20:28:19 dpchrist Exp $
 #######################################################################
 # package:
 #----------------------------------------------------------------------
@@ -20,7 +20,7 @@ our %EXPORT_TAGS = ( 'all' => [ qw(
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw();
 
-our $VERSION = sprintf("%d.%03d", q$Revision: 1.10 $ =~ /(\d+)/g);
+our $VERSION = sprintf("%d.%03d", q$Revision: 1.11 $ =~ /(\d+)/g);
 
 #######################################################################
 # uses:
@@ -38,7 +38,7 @@ Dpchrist::LangUtil - Perl language utilities
 
 =head1 DESCRIPTION
 
-This documentation describes module revision $Revision: 1.10 $.
+This documentation describes module revision $Revision: 1.11 $.
 
 
 This is alpha test level software
